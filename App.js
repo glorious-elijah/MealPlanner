5240ae6d903fb538dd4fd4cc41193ee573f4831e
Glorious <gloriouselijah125@gmail.com>
2023-12-17 14:45:16 +0000

    Add navigation and pages

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./pages/HomeScreen.tsx";
+import Settings from "./pages/Settings.tsx";
+import Favorites from "./pages/Favorites.tsx";
+import GroceryList from "./pages/GroceryList.tsx";
+import MealDetails from "./pages/MealDetails.tsx";
 
+const BottomTab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const AuxiliaryNavigation = () => {
+  <Stack.Navigator>
+    <Stack.Screen
+      name="MealDetails"
+      component={MealDetails}
+      options={({ route }) => ({ title: `Meal ${route.params.mealId}` })}
+    />
+  </Stack.Navigator>;
+};
+
+const MainNavigation = () => {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="Home" component={HomeScreen} />
+      <BottomTab.Screen name="GroceryList" component={GroceryList} />
+      <BottomTab.Screen name="Favorites" component={Favorites} />
+      <BottomTab.Screen name="Settings" component={Settings} />
+    </BottomTab.Navigator>
+  );
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MainNavigation />
+      <AuxiliaryNavigation />
     </View>
   );
 }
@@ -15,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ pages/GroceryList.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const GroceryList = (): JSX.Element => {
+  return (
+    <View>
+      <Text>GroceryList</Text>
+    </View>
+  );
+};
+
+export default GroceryList;
